9535dfa5ce9dba53e0f7e6fc911194b75b8abfea
UMAR-CUI <sp22-bcs-040@cuiatk.edu.pk>
2025-06-02 10:15:21 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
   public static void main(String[]args){
-    System.out.println("Hello from the pipeline");
+    System.out.println("Hello from the pipeline. Written after cahange");
   }
 }
